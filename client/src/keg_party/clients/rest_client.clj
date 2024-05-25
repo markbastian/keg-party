61196367e42442ebe358752bec0eb258e18be08d
Mark Bastian <markbastian@gmail.com>
2024-05-25 14:47:10 -0600

    Client updates + using fipp (#36)
    Made modifications to client code to allow for a nil port. Changed to fipp for faster object printing.

--- client/src/keg_party/clients/rest_client.clj
+++ client/src/keg_party/clients/rest_client.clj
@@ -1,9 +1,10 @@
 (ns keg-party.clients.rest-client
-  (:require [clojure.pprint :as pp]
-            [clojure.string :as str]
-            [clojure.tools.logging :as log]
-            [environ.core :refer [env]]
-            [hato.client :as hc])
+  (:require
+   [clojure.string :as str]
+   [clojure.tools.logging :as log]
+   [environ.core :refer [env]]
+   [fipp.edn :as fipp]
+   [hato.client :as hc])
   (:import (java.util Base64)))
 
 (defn base64-encode [s]
@@ -11,22 +12,25 @@
     (.encodeToString (Base64/getEncoder) (.getBytes s))))
 
 (defn post-tap-data
-  ([{:keys [username password]
+  ([{:keys [username password host port]
      :or   {username (or (env :keg-party-username) (env :user))
-            password (env :keg-party-password)}}
+            password (env :keg-party-password)
+            host     (env :keg-party-host "http://localhost")
+            port     (env :keg-party-port)}}
     data]
-   {:pre [username password]}
-   (let [host    (env :keg-party-host "http://localhost")
-         port    (env :keg-party-port "3333")
-         url     (cond-> host port (str ":" port))
-         body    (base64-encode (with-out-str (pp/pprint data)))
+   (let [url     (cond-> host
+                   (and port
+                        (or (number? port)
+                            (re-matches #"\d+" port)))
+                   (str ":" port))
+         body    (base64-encode (with-out-str (fipp/pprint data)))
          request {:url              url
                   :method           :post
                   :body             body
                   :basic-auth       {:user username
                                      :pass password}
                   :throw-exceptions false}
-         status (:status (hc/request request))]
+         status  (:status (hc/request request))]
      (case status
        200 (log/trace "Data posted to tap server.")
        401 (log/warnf
@@ -43,7 +47,9 @@
 
 (defn tap-in!
   ([] (add-tap post-tap-data))
-  ([username password] (add-tap (partial post-tap-data {:username username :password password}))))
+  ([username password] (add-tap (partial
+                                 post-tap-data
+                                 {:username username :password password}))))
 
 (defn tap-out! []
   (remove-tap post-tap-data))
@@ -51,11 +57,39 @@
 (comment
   (tap-in!)
 
-  (post-tap-data
-   '(let [host (env :keg-party-host "http://localhost")
-          port (env :keg-party-port "3333")
-          url  (cond-> host port (str ":" port))]
-      (hc/request
-       {:url    url
-        :method :post
-        :body   [1 2 3 4]}))))
+  ;; Example options with localhost and port config
+  (do
+    (post-tap-data
+     '(let [host (env :keg-party-host "http://localhost")
+            port (env :keg-party-port "3333")
+            url  (cond-> host port (str ":" port))]
+        (hc/request
+         {:url    url
+          :method :post
+          :body   [1 2 3 4]})))
+
+    (post-tap-data
+     {:host "http://localhost"
+      :port 3333}
+     {:strategy "post with localhost and long port"})
+
+    (post-tap-data
+     {:host "http://localhost"
+      :port "3333"}
+     {:strategy "post with localhost and string port"}))
+
+  ;; Example options when hitting a server directly
+  (do
+    (post-tap-data
+     {:host "https://keg-party.loca.lt"
+      :port nil}
+     {:strategy "post with lt and nil port"})
+
+    (post-tap-data
+     {:host "https://keg-party.loca.lt"}
+     {:strategy "post with lt and no port"})
+
+    (post-tap-data
+     {::host "https://keg-party.loca.lt"
+      :port  ""}
+     {:strategy "post with lt and empty string port"})))
